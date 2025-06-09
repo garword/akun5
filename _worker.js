8b22a2402c2b9751596063589a893648c8cd1985
garword <143305081+garword@users.noreply.github.com>
2025-06-09 07:57:59 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "gesaleiloiga-5825.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "pages.dev"; // Ganti dengan domain utama kalian
 const serviceName = "akun5"; // Ganti dengan nama workers kalian
 const apiKey = "Qa_TFo5NQhsDyj4xDuGSSlAaF4jPGd8QOCC4HXlf"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
